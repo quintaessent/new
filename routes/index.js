06dc4da339ab45fe50ac213ad46bafdbb034adb0
quintaessent <quintaessent@gmail.com>
2017-08-17 13:33:15 +0300

    blog/id

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,9 @@ router.get('/', function(req, res) {
 router.get('/blog', function(req, res) {
 	res.render('blog/blog');
 });
-router.get('/blog/:id?', function(req, res) {
+router.get('/blog/:id', function(req, res) {
 	var id = req.params.id;
-	if (id === "1"){
-		res.render('blog/1');
-	} else if (id === "2"){
-		res.render('blog/2');
-	} else {
-		res.render('blog/notfound');
-	}
+	res.render('blog/'+id);
 });
 
 module.exports = router;
