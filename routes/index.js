16157b58b50cb36f7076a7d1730c9ab471651852
quintaessent <quintaessent@gmail.com>
2017-08-17 16:44:21 +0300

    console

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('monk')('localhost/portfolioblog');
+const posts = db.get('posts');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 router.get('/blog', function(req, res) {
+	posts.find({}).then(function(response){
+		console.log(response);
+	});
 	res.render('blog/blog');
 });
 router.get('/blog/:id', function(req, res) {
 	var id = req.params.id;
+	console.log(id);
 	res.render('blog/'+id);
 });
 
