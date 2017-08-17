7eca8de81788b5fe113e10103ff70b87f3e1f9fa
quintaessent <quintaessent@gmail.com>
2017-08-17 18:55:27 +0300

    ewsponse

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 router.get('/blog', function(req, res) {
-	posts.find({}).then(function(response){
-		console.log(response);
+	res.render('blog/blog', {
+		posts: response
 	});
-	res.render('blog/blog');
 });
 router.get('/blog/:id', function(req, res) {
 	var id = req.params.id;
