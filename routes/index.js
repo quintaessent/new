dd51397268dcd5c85c69afc143659dbde9cafe76
quintaessent <quintaessent@gmail.com>
2017-08-27 15:40:13 +0300

    newlines

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,26 @@ const posts = db.get('posts');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { header_class: 'ytp-video-bg', header_id: 'header-page'});
+});
+router.get('/contacts', function(req, res) {
+	res.render('contacts', {header_class: 'contact-page', text: 'Contact'});
+});
+router.get('/album', function(req, res) {
+	res.render('album', {header_class: 'albums-page', text: 'Albums'});
+});
+router.get('/project', function(req, res) {
+	res.render('project', {header_class: 'projekt-page', text: 'Albums'});
 });
 router.get('/blog', function(req, res) {
 	posts.find({}).then(function(response){
-		res.render('blog', {posts: response});
+		res.render('blog', {posts: response, header_class: 'blog-home-page', text: 'Blog'});
 	});
 });
 router.get('/blog/:id', function(req, res) {
 	var id = req.params.id;
-	console.log(req)
 	posts.findOne({slug:req.params.id}).then(function(response){
-		res.render('post', {post: response});
+		res.render('post', {post: response, header_class: 'blog-home-page', text: 'Single post'});
 	});
 });
 
